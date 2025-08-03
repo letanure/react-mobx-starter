54cdd31b1e582e9e4c437007282deb0eb82bd07b
letanure <letanure@gmail.com>
2025-08-03 17:38:27 +0200

    refactor: use vitest config on vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,25 +1,8 @@
-import path from "node:path"
-import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vitest/config"
+import viteConfig from "./vite.config"
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@/components": path.resolve(__dirname, "./src/components"),
-      "@/hooks": path.resolve(__dirname, "./src/hooks"),
-      "@/utils": path.resolve(__dirname, "./src/utils"),
-      "@/types": path.resolve(__dirname, "./src/types"),
-      "@/lib": path.resolve(__dirname, "./src/lib"),
-      "@/stores": path.resolve(__dirname, "./src/stores"),
-      "@/providers": path.resolve(__dirname, "./src/providers"),
-      "@/features": path.resolve(__dirname, "./src/features"),
-      "@/assets": path.resolve(__dirname, "./src/assets"),
-      "@/constants": path.resolve(__dirname, "./src/constants"),
-      "@/contexts": path.resolve(__dirname, "./src/contexts"),
-      "@/styles": path.resolve(__dirname, "./src/styles"),
-    },
-  },
+  ...viteConfig,
   test: {
     globals: true,
     environment: "jsdom",
